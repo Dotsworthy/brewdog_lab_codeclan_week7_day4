643e66c6268b15b27dcd2b29e86a5a3d25ec65e1
Dotsworthy <dotsworthy@gmail.com>
2020-02-27 21:01:08 +0000

    added ingredients List

--- src/components/BeerDetail.vue
+++ src/components/BeerDetail.vue
@@ -3,6 +3,7 @@
 <h2>{{beer.name}}</h2>
 <p>{{beer.tagline}}</p>
 <p>{{beer.abv}}% AVB</p>
+<p>Ingredients: {{getIngredients()}}</p>
 <button v-on:click="addToFavourites">Add Beer</button>
 <button v-on:click="removeFromFavourites" > Remove from Favourites </button>
 </div>
@@ -15,14 +16,34 @@ export default {
   name:'beer-detail',
   props:['beer', 'favouriteBeers'],
   methods: {
-    addToFavourites(){
+    addToFavourites() {
       eventBus.$emit("beer-favourited", this.beer)
     },
     removeFromFavourites(){
       eventBus.$emit("beer-to-remove", this.beer)
+    },
+    getIngredients() {
+      let ingredientArray = []
+      let allHops = this.beer.ingredients.hops.map((hop) => {
+        return hop.name;
+      });
+      let filteredHops = allHops.filter((hop,index) => {
+        return allHops.indexOf(hop) >= index
+      });
+      let allMalt = this.beer.ingredients.malt.map((malt) => {
+        return malt.name;
+      });
+      let filteredMalt = allMalt.filter((malt,index) => {
+        return allMalt.indexOf(malt) >= index
+      });
+      ingredientArray = filteredHops.concat(filteredMalt);
+      ingredientArray.push(this.beer.ingredients.yeast)
+      let result = ingredientArray.join(', ');
+      return result;
     }
   }
 }
+
 </script>
 
 <style lang="css" scoped>
--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -8,13 +8,12 @@ import {eventBus} from '../main.js'
 export default {
   name: 'list-item',
   props: ['beer'],
-  methods:{
+  methods: {
     handleClick(){
       eventBus.$emit('beer-selected',this.beer)
     }
   }
 }
-
 
 </script>
 <style lang="css" scoped>
